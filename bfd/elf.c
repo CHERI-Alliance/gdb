3b187d666cfcec1346ae8d8f7a3681b3435a6090
John Baldwin <jhb@FreeBSD.org>
2017-03-01 15:54:27 -0800

    Recognize capreg note and add a psuedo-section for GDB to find.

--- bfd/elf.c
+++ bfd/elf.c
@@ -11532,6 +11532,9 @@ elfcore_grok_freebsd_note (bfd *abfd, Elf_Internal_Note *note)
     case NT_FREEBSD_X86_SEGBASES:
       return elfcore_make_note_pseudosection (abfd, NOTE_PSEUDO_SECTION_X86_SEGBASES, note);
 
+    case NT_FREEBSD_CAPREGS:
+      return elfcore_make_note_pseudosection (abfd, ".reg-cap", note);
+
     case NT_X86_XSTATE:
       return elfcore_grok_xstatereg (abfd, note);
 
