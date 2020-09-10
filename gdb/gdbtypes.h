1f895df8692920820a70690f006dff4e37936b50
Luis Machado <luis.machado@arm.com>
2020-09-10 17:33:06 -0300

    [General] Accept capabilities as a type of pointer
    This makes GDB happy when trying to convert to/from capabilities from
    long types.
    
    gdb/ChangeLog:
    
    2020-10-20  Luis Machado  <luis.machado@arm.com>
    
    	* findvar.c (extract_typed_address): Handle capabilities.
    	(store_typed_address): Likewise.
    	* value.c (unpack_long): Likewise.
    	(pack_long): Likewise.

--- gdb/gdbtypes.h
+++ gdb/gdbtypes.h
@@ -1522,7 +1522,8 @@ struct type
   /* Return true if this is a pointer or reference type.  */
   bool is_pointer_or_reference () const
   {
-    return this->code () == TYPE_CODE_PTR || TYPE_IS_REFERENCE (this);
+    return this->code () == TYPE_CODE_PTR
+      || this->code () == TYPE_CODE_CAPABILITY || TYPE_IS_REFERENCE (this);
   }
 
   /* Return true if this type is "string-like", according to its
