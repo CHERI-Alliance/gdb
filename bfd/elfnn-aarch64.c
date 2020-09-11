abf18264e32506edb06775fdc3e1635bfb2015a1
Siddhesh Poyarekar <siddesh.poyarekar@arm.com>
2020-09-11 09:18:05 +0530

    [AArch64] Remove section caches for section data
    The cache is not used anymore and is unnecessarily allocated and
    freed.
    
    bfd/ChangeLog:
    
    2020-10-20  Siddhesh Poyarekar  <siddesh.poyarekar@arm.com>
    
    	* elfnn-aarch64.c (section_list,
    	sections_with_aarch64_elf_section_data,
    	record_section_with_aarch64_elf_section_data,
    	find_aarch64_elf_section_entry,
    	unrecord_section_with_aarch64_elf_section_data,
    	unrecord_section_via_map_over_sections,
    	bfd_elfNN_close_and_cleanup, bfd_elfNN_bfd_free_cached_info):
    	Remove.  Adjust all users.

--- bfd/elfnn-aarch64.c
+++ bfd/elfnn-aarch64.c
@@ -8702,12 +8702,16 @@ elfNN_aarch64_output_arch_local_syms (bfd *output_bfd,
 static bool
 elfNN_aarch64_new_section_hook (bfd *abfd, asection *sec)
 {
-  _aarch64_elf_section_data *sdata;
+  if (!sec->used_by_bfd)
+    {
+      _aarch64_elf_section_data *sdata;
+      size_t amt = sizeof (*sdata);
 
-  sdata = bfd_zalloc (abfd, sizeof (*sdata));
-  if (sdata == NULL)
-    return false;
-  sec->used_by_bfd = sdata;
+      sdata = bfd_zalloc (abfd, amt);
+      if (sdata == NULL)
+	      return false;
+      sec->used_by_bfd = sdata;
+    }
 
   return _bfd_elf_new_section_hook (abfd, sec);
 }
