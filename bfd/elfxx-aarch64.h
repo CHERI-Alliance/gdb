fce0278e2269857fc258f98d8e9c477cdbc0046d
Siddhesh Poyarekar <siddesh.poyarekar@arm.com>
2020-09-11 09:18:11 +0530

    [Morello] Pad section alignment to account for capability range format
    The capability format has limitations on the alignment and length of
    capability bounds and are subject to rounding.  Add alignment and
    padding at the boundaries of such long (typically >16M) sections so
    that any capabilities referencing these sections do not end up
    overlapping into neighbouring sections.
    
    There are two cases where this is in use.  The first and most
    important due to the current implementation is the range for PCC,
    which needs to span all executable sections and all PLT and GOT
    sections.  The other case is for linker and ldscript defined symbols
    that may be used in dynamic relocations.
    
    bfd/ChangeLog:
    
    2020-10-20  Siddhesh Poyarekar  <siddesh.poyarekar@arm.com>
    
    	* elfnn-aarch64.c (elf_aarch64_link_hash_table): New member.
    	(section_start_symbol, c64_valid_cap_range, exponent): Move
    	up.
    	(sec_change_queue): New structure.
    	(queue_section_padding, record_section_change,
    	elfNN_c64_resize_sections): New functions.
    	(bfd_elfNN_aarch64_init_maps): Add info argument.  Adjust
    	callers.
    	* elfxx-aarch64.h (bfd_elf64_aarch64_init_maps,
    	bfd_elf32_aarch64_init_maps): Add info argument.
    	(elf64_c64_resize_sections, elf32_c64_resize_sections): New
    	function declarations.
    
    ld/ChangeLog:
    
    2020-10-20  Siddhesh Poyarekar  <siddesh.poyarekar@arm.com>
    
    	* emultempl/aarch64elf.em (elf64_c64_pad_section): New
    	function.
    	(gld${EMULATION_NAME}_after_allocation): Resize C64 sections.
    	* ldlang.c (lang_add_newdot): New function.
    	* ldlang.h (lang_add_newdot): New function declaration.
    	* testsuite/ld-aarch64/aarch64-elf.exp: Add new test.
    	* testsuite/ld-aarch64/morello-sec-round.d: New file.
    	* testsuite/ld-aarch64/morello-sec-round.ld: New file.
    	* testsuite/ld-aarch64/morello-sec-round.s: New file.

--- bfd/elfxx-aarch64.h
+++ bfd/elfxx-aarch64.h
@@ -19,10 +19,10 @@
    see <http://www.gnu.org/licenses/>.  */
 
 extern void bfd_elf64_aarch64_init_maps
-  (bfd *);
+  (bfd *, struct bfd_link_info *);
 
 extern void bfd_elf32_aarch64_init_maps
-  (bfd *);
+  (bfd *, struct bfd_link_info *);
 
 /* Types of PLTs based on the level of security.  This would be a
    bit-mask to denote which of the combinations of security features
@@ -171,6 +171,11 @@ extern bool elf64_aarch64_size_stubs
   (bfd *, bfd *, struct bfd_link_info *, bfd_signed_vma,
    struct bfd_section * (*) (const char *, struct bfd_section *),
    void (*) (void));
+
+extern void elf64_c64_resize_sections (bfd *, struct bfd_link_info *,
+				       void (*) (asection *, bfd_vma),
+				       void (*) (void));
+
 extern bool elf64_aarch64_build_stubs
   (struct bfd_link_info *);
 /* AArch64 stub generation support for ELF32.  Called from the linker.  */
@@ -195,6 +200,10 @@ extern bool elf32_aarch64_size_relative_relocs
   (struct bfd_link_info *, bool *);
 extern bool elf32_aarch64_finish_relative_relocs
   (struct bfd_link_info *);
+
+extern void elf32_c64_resize_sections (bfd *, struct bfd_link_info *,
+				       void (*) (asection *, bfd_vma),
+				       void (*) (void));
 
 /* Take the PAGE component of an address or offset.  */
 #define PG(x)	     ((x) & ~ (bfd_vma) 0xfff)
