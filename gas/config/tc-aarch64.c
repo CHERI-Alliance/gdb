3a7fb4cc0134b5f53c22fbc9f8d43cdea6cb9320
Siddhesh Poyarekar <siddesh.poyarekar@arm.com>
2020-09-11 09:18:10 +0530

    [Morello] Implement branch relocations
    This implements the following static relocations:
    
    - R_MORELLO_CALL26, R_MORELLO_JUMP26
    - R_MORELLO_TSTBR14, R_MORELLO_CONDBR19
    
    and the following dynamic relocations:
    
    - R_MORELLO_JUMP_SLOT and R_MORELLO_IRELATIVE
    
    Some notes on the implementation:
    
    - The linker selects morello PLT stubs when it finds at least one
      static relocation that needs a capability GOT slot.
    
    - It is assumed that C64 is not compatible with BTI/PAC, so the latter
      gets overridden.  To allow this, the call to setup_plt_values is
      delayed to take into account htab->c64_plt.
    
    - If the caller is A64, the assembler emits R_AARCH64_JUMP_SLOT,
      otherwise it emits R_MORELLO_JUMP_SLOT.
    
    - The PLT stub is A64-compatible, in that it should do the right thing
      when the execution state is A64.
    
    - If the slots are 16-bytes (this happens when there is at least one
      Morello relocation on the GOT), the references in .plt.got and in
      .got are always capabilities; the dynamic linker will take care of
      that.  For PLT, the default trampoline is a capability.  This is
      true for A64 as well as C64.
    
    - At present it is assumed that there is no interworking between A64
      and C64 functions.
    
    bfd/ChangeLog:
    
    2020-10-20  Siddhesh Poyarekar  <siddesh.poyarekar@arm.com>
    
    	* elfnn-aarch64.c (elfNN_c64_small_plt0_entry,
    	elfNN_c64_small_plt_entry): New variables.
    	(elfNN_aarch64_howto_table): Add relocations.
    	(setup_plt_values): Choose C64 PLT when appropriate.
    	(bfd_elfNN_aarch64_set_options): Defer setup_plt_values
    	call...
    	(elfNN_aarch64_link_setup_gnu_properties) ... from here as
    	well...
    	(elfNN_aarch64_size_dynamic_sections): ... to here.
    	(elfNN_aarch64_final_link_relocate,
    	elfNN_aarch64_check_relocs, elfNN_aarch64_reloc_type_class):
    	Support new relocations.
    	(map_symbol_type): New member AARCH64_MAP_C64.
    	(elfNN_aarch64_output_arch_local_syms): Use it.
    	(aarch64_update_c64_plt_entry): New function.
    	(elfNN_aarch64_create_small_pltn_entry): Use it.
    	(elfNN_aarch64_init_small_plt0_entry): Emit C64 PLT when
    	appropriate.
    	* elfxx-aarch64.c (_bfd_aarch64_elf_put_addend,
    	_bfd_aarch64_elf_resolve_relocation): Add new relocations.
    	* libbfd.h (bfd_reloc_code_real_names): Likewise.
    	* reloc.c: New relocations BFD_RELOC_MORELLO_TSTBR14,
    	BFD_RELOC_MORELLO_BRANCH19, BFD_RELOC_MORELLO_JUMP26,
    	BFD_RELOC_MORELLO_CALL26, BFD_RELOC_MORELLO_JUMP_SLOT and
    	BFD_RELOC_MORELLO_IRELATIVE.
    	* bfd-in2.h: Regenerate.
    
    gas/ChangeLog:
    
    2020-10-20  Siddhesh Poyarekar  <siddesh.poyarekar@arm.com>
    
    	* config/tc-aarch64.c (parse_operands): Choose C64 branch
    	relocations when appropriate.
    	(md_apply_fix, aarch64_force_relocation,
    	aarch64_fix_adjustable): Support C64 branch relocations.
    
    include/ChangeLog:
    
    2020-10-20  Siddhesh Poyarekar  <siddesh.poyarekar@arm.com>
    
    	* elf/aarch64.h: New relocations R_MORELLO_TSTBR14,
    	R_MORELLO_CONDBR19, R_MORELLO_JUMP26, R_MORELLO_CALL26,
    	R_MORELLO_JUMP_SLOT and R_MORELLO_IRELATIVE.
    
    ld/ChangeLog:
    
    2020-10-20  Siddhesh Poyarekar  <siddesh.poyarekar@arm.com>
    
    	* testsuite/ld-aarch64/aarch64-elf.exp: Add new tests.
    	* testsuite/ld-aarch64/c64-ifunc-2-local.d: New file.
    	* testsuite/ld-aarch64/c64-ifunc-2.d: New file.
    	* testsuite/ld-aarch64/c64-ifunc-3a.d: New file.
    	* testsuite/ld-aarch64/c64-ifunc-3b.d: New file.
    	* testsuite/ld-aarch64/c64-ifunc-4.d: New file.
    	* testsuite/ld-aarch64/c64-ifunc-4a.d: New file.
    	* testsuite/ld-aarch64/ifunc-2-local.s: Support capabilities.
    	* testsuite/ld-aarch64/ifunc-2.s: Likewise.
    
    opcodes/ChangeLog:
    
    2020-10-20  Siddhesh Poyarekar  <siddesh.poyarekar@arm.com>
    
    	* aarch64-dis.c (get_sym_code_type): Fix C64 PLT disassembly.

--- gas/config/tc-aarch64.c
+++ gas/config/tc-aarch64.c
@@ -3658,8 +3658,12 @@ aarch64_force_reloc (struct fix *fixp)
       return 1;
 
     case BFD_RELOC_AARCH64_ADR_LO21_PCREL:
+    case BFD_RELOC_MORELLO_BRANCH19:
+    case BFD_RELOC_MORELLO_TSTBR14:
     case BFD_RELOC_AARCH64_BRANCH19:
     case BFD_RELOC_AARCH64_TSTBR14:
+    case BFD_RELOC_MORELLO_CALL26:
+    case BFD_RELOC_MORELLO_JUMP26:
     case BFD_RELOC_AARCH64_CALL26:
     case BFD_RELOC_AARCH64_JUMP26:
       gas_assert (fixp->fx_addsy != NULL);
@@ -7960,21 +7964,31 @@ bad_adrdp:
 		    /* e.g. CBZ or B.COND  */
 		    gas_assert (operands[i] == AARCH64_OPND_ADDR_PCREL9
 				|| operands[i] == AARCH64_OPND_ADDR_PCREL19);
-		    inst.reloc.type = (operands[i] == AARCH64_OPND_ADDR_PCREL9)
+		    inst.reloc.type = (c64 ? BFD_RELOC_MORELLO_BRANCH19
+				       : (operands[i] == AARCH64_OPND_ADDR_PCREL9)
 				       ? BFD_RELOC_AARCH64_BRANCH9
-				       : BFD_RELOC_AARCH64_BRANCH19;
+				       : BFD_RELOC_AARCH64_BRANCH19);
 		    break;
 		  case testbranch:
 		    /* e.g. TBZ  */
 		    gas_assert (operands[i] == AARCH64_OPND_ADDR_PCREL14);
-		    inst.reloc.type = BFD_RELOC_AARCH64_TSTBR14;
+		    inst.reloc.type = (c64 ? BFD_RELOC_MORELLO_TSTBR14
+				       : BFD_RELOC_AARCH64_TSTBR14);
 		    break;
 		  case branch_imm:
 		    /* e.g. B or BL  */
-		    gas_assert (operands[i] == AARCH64_OPND_ADDR_PCREL26);
-		    inst.reloc.type =
-		      (opcode->op == OP_BL) ? BFD_RELOC_AARCH64_CALL26
-			 : BFD_RELOC_AARCH64_JUMP26;
+		      {
+			enum aarch64_opnd jump, call;
+
+			gas_assert (operands[i] == AARCH64_OPND_ADDR_PCREL26);
+
+			jump = (c64 ? BFD_RELOC_MORELLO_JUMP26
+				: BFD_RELOC_AARCH64_JUMP26);
+			call = (c64 ? BFD_RELOC_MORELLO_CALL26
+				: BFD_RELOC_AARCH64_CALL26);
+
+			inst.reloc.type = opcode->op == OP_BL ? call : jump;
+		      }
 		    break;
 		  case loadlit:
 		    gas_assert (operands[i] == AARCH64_OPND_ADDR_PCREL19
@@ -10343,6 +10357,7 @@ md_apply_fix (fixS * fixP, valueT * valP, segT seg)
 	}
       break;
     case BFD_RELOC_AARCH64_BRANCH19:
+    case BFD_RELOC_MORELLO_BRANCH19:
       if (fixP->fx_done || !seg->use_rela_p)
 	{
 	  if (value & 3)
@@ -10357,6 +10372,7 @@ md_apply_fix (fixS * fixP, valueT * valP, segT seg)
 	}
       break;
 
+    case BFD_RELOC_MORELLO_TSTBR14:
     case BFD_RELOC_AARCH64_TSTBR14:
       if (fixP->fx_done || !seg->use_rela_p)
 	{
@@ -10372,6 +10388,8 @@ md_apply_fix (fixS * fixP, valueT * valP, segT seg)
 	}
       break;
 
+    case BFD_RELOC_MORELLO_CALL26:
+    case BFD_RELOC_MORELLO_JUMP26:
     case BFD_RELOC_AARCH64_CALL26:
     case BFD_RELOC_AARCH64_JUMP26:
       if (fixP->fx_done || !seg->use_rela_p)
@@ -10904,6 +10922,10 @@ aarch64_fix_adjustable (struct fix *fixP)
     case BFD_RELOC_AARCH64_TSTBR14:
     case BFD_RELOC_AARCH64_JUMP26:
     case BFD_RELOC_AARCH64_CALL26:
+    case BFD_RELOC_MORELLO_BRANCH19:
+    case BFD_RELOC_MORELLO_TSTBR14:
+    case BFD_RELOC_MORELLO_JUMP26:
+    case BFD_RELOC_MORELLO_CALL26:
       if (fixP->tc_fix_data.c64 || AARCH64_IS_C64 (fixP->fx_addsy))
 	return false;
       break;
