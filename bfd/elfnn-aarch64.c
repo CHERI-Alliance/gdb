0605b772b22efacba161026c27911fd57ac4ad6a
Matthew Malcomson <matthew.malcomson@arm.com>
2021-09-20 16:29:01 +0100

    morello-binutils: Adjust c64_valid_cap_range calculation
    This function had a buggy implementation of rounding a value up to a
    given power of 2.  Aligning to a multiple of 16 would align to a
    multiple of 32 and so on.
    
    This was observable when linking object files that had very large
    objects in them.  The compiler would ensure that these objects are large
    enough that they are exactly representable, but the linker would
    complain that they are not because the linker asserted extra alignment
    than the compiler.
    
    Here we fix the bug, add a few testcases, and adjust an existing
    testcase in the area.

--- bfd/elfnn-aarch64.c
+++ bfd/elfnn-aarch64.c
@@ -4927,7 +4927,7 @@ exponent (uint64_t len)
 #undef CAP_MAX_EXPONENT
 }
 
-#define ONES(x)         ((1ULL << ((x) + 1)) - 1)
+#define ONES(x)         ((1ULL << (x)) - 1)
 #define ALIGN_UP(x, a)  (((x) + ONES (a)) & (~ONES (a)))
 
 static bool
