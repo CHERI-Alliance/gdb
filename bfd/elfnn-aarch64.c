780527c6f61907a7bce4adc8b68158ad0d8ee6e3
Matthew Malcomson <matthew.malcomson@arm.com>
2022-01-18 10:54:13 +0000

    Switch __cap_dynrelocs* to __rela_dyn* symbols
    The name has been changed in LLVM, so we adjust it in binutils to match.
    
    We also move where these symbols are created.  Previously they were
    created in elfNN_aarch64_always_size_sections, but we move this to
    elfNN_aarch64_size_dynamic_sections.
    
    We do the moving since these symbols are supposed to span all dynamic
    capability relocations stored in the .rela.dyn section for static
    executables.  In the case of a static binary we place relocations for
    the GOT into this section as well as internal relocations.
    
    These relocations for the GOT are handled in
    elfNN_aarch64_size_dynamic_sections, which is called *after*
    elfNN_aarch64_always_size_sections.  The size of this section is only
    fully known after those GOT relocations are managed, so the position
    these symbols should be placed in is only known at that point.  Hence we
    only initialise the __rela_dyn* symbols at that point.
    
    2021-10-06  Matthew Malcomson  <matthew.malcomson@arm.com>
    ChangeLog:
    
    	* bfd/elfnn-aarch64.c (elfNN_aarch64_always_size_sections): Move
    	initialisation of __rela_dyn* symbols ...
    	(elfNN_aarch64_size_dynamic_sections): ... to here.
    	* ld/testsuite/ld-aarch64/aarch64-elf.exp: Run new tests.
    	* ld/testsuite/ld-aarch64/emit-morello-reloc-markers-1.d: New test.
    	* ld/testsuite/ld-aarch64/emit-morello-reloc-markers-1.s: New test.
    	* ld/testsuite/ld-aarch64/emit-morello-reloc-markers-2.d: New test.
    	* ld/testsuite/ld-aarch64/emit-morello-reloc-markers-2.s: New test.
    	* ld/testsuite/ld-aarch64/emit-morello-reloc-markers-3.d: New test.
    	* ld/testsuite/ld-aarch64/emit-morello-reloc-markers-3.s: New test.

--- bfd/elfnn-aarch64.c
+++ bfd/elfnn-aarch64.c
@@ -11140,6 +11140,23 @@ elfNN_aarch64_late_size_sections (bfd *output_bfd ATTRIBUTE_UNUSED,
 		 elfNN_aarch64_allocate_local_ifunc_dynrelocs,
 		 info);
 
+  if (bfd_link_executable (info)
+      && !bfd_link_pic (info)
+      && htab->srelcaps
+      && htab->srelcaps->size > 0)
+    {
+      struct elf_link_hash_entry *h;
+
+      h = _bfd_elf_define_linkage_sym (output_bfd, info,
+				       htab->srelcaps,
+				       "__rela_dyn_start");
+      h = _bfd_elf_define_linkage_sym (output_bfd, info,
+				       htab->srelcaps,
+				       "__rela_dyn_end");
+
+      h->root.u.def.value = htab->srelcaps->vma + htab->srelcaps->size;
+    }
+
   /* For every jump slot reserved in the sgotplt, reloc_count is
      incremented.  However, when we reserve space for TLS descriptors,
      it's not incremented, so in order to compute the space reserved
@@ -11521,25 +11538,6 @@ elfNN_aarch64_early_size_sections (bfd *output_bfd,
 
   if (bfd_link_relocatable (info))
     return true;
-
-  struct elf_aarch64_link_hash_table *htab = elf_aarch64_hash_table (info);
-
-  if (bfd_link_executable (info)
-      && !bfd_link_pic (info)
-      && htab->srelcaps
-      && htab->srelcaps->size > 0)
-    {
-      struct elf_link_hash_entry *h;
-
-      h = _bfd_elf_define_linkage_sym (output_bfd, info,
-				       htab->srelcaps,
-				       "__cap_dynrelocs_start");
-      h = _bfd_elf_define_linkage_sym (output_bfd, info,
-				       htab->srelcaps,
-				       "__cap_dynrelocs_end");
-
-      h->root.u.def.value = htab->srelcaps->vma + htab->srelcaps->size;
-    }
 
   tls_sec = elf_hash_table (info)->tls_sec;
 
