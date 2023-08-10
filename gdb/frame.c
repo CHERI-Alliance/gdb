99f31a79725dc0e80c73e5d33ea0910a51b6b46d
John Baldwin <jhb@FreeBSD.org>
2023-08-10 09:44:34 -0700

    regcache: Save/restore register tags

--- gdb/frame.c
+++ gdb/frame.c
@@ -1115,8 +1115,18 @@ frame_save_as_regcache (const frame_info_ptr &this_frame)
 	return REG_VALID;
     };
 
+  auto read_tag = [this_frame] (int regnum)
+    {
+      struct value *val = get_frame_register_value (this_frame, regnum);
+      if (val != nullptr && val->tagged ())
+	return val->tag ();
+      else
+	return false;
+    };
+
   std::unique_ptr<readonly_detached_regcache> regcache
-    (new readonly_detached_regcache (get_frame_arch (this_frame), cooked_read));
+    (new readonly_detached_regcache (get_frame_arch (this_frame), cooked_read,
+				     read_tag));
 
   return regcache;
 }
