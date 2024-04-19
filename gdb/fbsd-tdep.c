8434b0061ce715d78644cea3efa74d6e9396c9d6
John Baldwin <jhb@FreeBSD.org>
2024-04-18 17:02:03 -0700

    fbsd-tdep: Export fbsd_read_integer_by_name

--- gdb/fbsd-tdep.c
+++ gdb/fbsd-tdep.c
@@ -2007,9 +2007,9 @@ fbsd_get_syscall_number (struct gdbarch *gdbarch, thread_info *thread)
   internal_error (_("fbsd_get_sycall_number called"));
 }
 
-/* Read an integer symbol value from the current target.  */
+/* See fbsd-tdep.h.  */
 
-static LONGEST
+LONGEST
 fbsd_read_integer_by_name (struct gdbarch *gdbarch, const char *name)
 {
   bound_minimal_symbol ms
