aa4fd843e37db36ad22d0a991d70bf7be7fe3498
John Baldwin <jhb@FreeBSD.org>
2024-06-12 13:28:50 -0700

    Use is_capability to determine if types are capabilities
    In most places this is a no-op, but a few places were only checking
    for scalar or pointers but not both.  This is also more future proof
    against recently found bugs with typedef'd capability types not being
    handled properly.

--- gdb/aarch64-tdep.c
+++ gdb/aarch64-tdep.c
@@ -2377,7 +2377,7 @@ pass_in_c_x_or_stack (struct gdbarch *gdbarch, struct regcache *regcache,
 
   /* Check if we have a case where we need to pass arguments via the C
      registers.  */
-  if (TYPE_CAPABILITY (type) || type->contains_capability ())
+  if (is_capability (type) || type->contains_capability ())
     pass_in_c_or_stack (gdbarch, regcache, info, type, arg);
   else
     pass_in_x_or_stack (gdbarch, regcache, info, type, arg);
@@ -2725,10 +2725,10 @@ convert_pointer_to_capability (struct value *source, CORE_ADDR pointer)
 {
   aarch64_debug_enter_exit ();
 
-  gdb_assert (TYPE_CAPABILITY (source->type ()));
-
   if (source == nullptr)
     return nullptr;
+
+  gdb_assert (is_capability (source->type ()));
 
   if (source->contents ().data () == nullptr)
     return nullptr;
@@ -3922,11 +3922,9 @@ morello_extract_return_value (struct value *value, struct regcache *regs,
     {
       aarch64_debug_printf ("Pointer/Capability types");
 
-      bool has_capability = TYPE_CAPABILITY (type)
-	|| type->code () == TYPE_CODE_CAPABILITY;
       int regno;
 
-      if (has_capability)
+      if (is_capability (type))
 	{
 	  regno = tdep->cap_reg_base + AARCH64_X0_REGNUM;
 	  value->set_tag (regs->raw_collect_tag (regno));
@@ -4365,13 +4363,11 @@ morello_store_return_value (struct value *value, struct regcache *regs,
 	   || type->code () == TYPE_CODE_CAPABILITY
 	   || TYPE_IS_REFERENCE (type))
     {
-      bool has_capability = TYPE_CAPABILITY (type)
-	|| type->code () == TYPE_CODE_CAPABILITY;
       int regno;
 
       aarch64_debug_printf ("Pointer/Capability types");
 
-      if (has_capability)
+      if (is_capability (type))
 	{
 	  regno = tdep->cap_reg_base + AARCH64_X0_REGNUM;
 	  regs->raw_supply_tag (regno, value->tag ());
--- gdb/frame.c
+++ gdb/frame.c
@@ -1410,7 +1410,7 @@ frame_unwind_register_unsigned (const frame_info_ptr &next_frame, int regnum)
     }
 
   ULONGEST r;
-  if (type->code () == TYPE_CODE_CAPABILITY || TYPE_CAPABILITY (type))
+  if (is_capability (type))
     r = gdbarch_pointer_to_address (gdbarch, type,
 				    value->contents_all ().data ());
   else
