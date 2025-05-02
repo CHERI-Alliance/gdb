2a2504a7e7fea15d6fc7a6b57f5b6451cc0d48c1
John Baldwin <jhb@FreeBSD.org>
2025-05-02 15:49:03 -0400

    comparts: Drop imports and trusts from struct compart
    These aren't used currently and might be implemented differently in
    the future.

--- gdb/aarch64-fbsd-tdep.c
+++ gdb/aarch64-fbsd-tdep.c
@@ -623,8 +623,6 @@ struct fbsd_comparts_data
   LONGEST compart_size = 0;
   LONGEST compart_name_off = 0;
   LONGEST compart_libs_off = 0;
-  LONGEST compart_imports_off = 0;
-  LONGEST compart_trusts_off = 0;
 
   /* Additional fields in r_debug.  */
   LONGEST r_comparts_size_off = 0;
@@ -665,10 +663,6 @@ aarch64_fbsd_fetch_compart_offsets (struct gdbarch *gdbarch,
 						  "name", 0).offset / 8;
       data->compart_libs_off = lookup_struct_elt (compart_sym->type (),
 						  "libs", 0).offset / 8;
-      data->compart_imports_off = lookup_struct_elt (compart_sym->type (),
-						     "imports", 0).offset / 8;
-      data->compart_trusts_off = lookup_struct_elt (compart_sym->type (),
-						    "trusts", 0).offset / 8;
       data->compart_size = compart_sym->type ()->length ();
 
       data->r_comparts_size_off = 84;
@@ -688,8 +682,6 @@ aarch64_fbsd_fetch_compart_offsets (struct gdbarch *gdbarch,
       /* Assume default layout.  */
       data->compart_name_off = 0;
       data->compart_libs_off = 16;
-      data->compart_imports_off = 48;
-      data->compart_trusts_off = 80;
 
       data->r_comparts_size_off = 84;
       data->r_comparts_off = 96;
@@ -789,26 +781,6 @@ aarch64_fbsd_fetch_compart_info (struct gdbarch *gdbarch,
     {
       c->libraries
 	= aarch64_fetch_string_base (gdbarch, c->addr + data->compart_libs_off);
-    }
-  catch (const gdb_exception_error &e)
-    {
-    }
-
-  try
-    {
-      c->imports
-	= aarch64_fetch_string_base (gdbarch,
-				     c->addr + data->compart_imports_off);
-    }
-  catch (const gdb_exception_error &e)
-    {
-    }
-
-  try
-    {
-      c->trusts
-	= aarch64_fetch_string_base (gdbarch,
-				     c->addr + data->compart_trusts_off);
     }
   catch (const gdb_exception_error &e)
     {
