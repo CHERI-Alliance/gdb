695b91e30e8d90bee949ffdd5ede3ae06df4e93b
John Baldwin <jhb@FreeBSD.org>
2026-02-19 12:12:31 -0500

    loongarch-tdep.c: Cast integer to uintptr_t before void *
    This quiets warnings on CHERI platforms

--- gdb/loongarch-tdep.c
+++ gdb/loongarch-tdep.c
@@ -2167,14 +2167,14 @@ loongarch_gdbarch_init (struct gdbarch_info info, struct gdbarch_list *arches)
   for (int i = 0; i < ARRAY_SIZE (loongarch_r_alias); ++i)
     if (loongarch_r_alias[i][0] != '\0')
       user_reg_add (gdbarch, loongarch_r_alias[i] + 1,
-	value_of_loongarch_user_reg, (void *) (size_t) i);
+	value_of_loongarch_user_reg, (void *) (uintptr_t) i);
 
   for (int i = 0; i < ARRAY_SIZE (loongarch_f_alias); ++i)
     {
       if (loongarch_f_alias[i][0] != '\0')
 	user_reg_add (gdbarch, loongarch_f_alias[i] + 1,
 		      value_of_loongarch_user_reg,
-		      (void *) (size_t) (LOONGARCH_FIRST_FP_REGNUM + i));
+		      (void *) (uintptr_t) (LOONGARCH_FIRST_FP_REGNUM + i));
     }
 
   /* Information about registers.  */
