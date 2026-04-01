bminor/binutils-gdb

Print capability pointers as capabilities
Print capability pointers as capabilities, so the user can see the
decoded fields of the capability.

gdb/ChangeLog:

2020-11-11  Luis Machado  <luis.machado@arm.com>

	* c-valprint.c (c_value_print_ptr): Adjust to print capability
	pointers as capabilities.
	(c_value_print_inner): Handle TYPE_CODE_CAPABILITY.
	* findvar.c (extract_integer): Truncate scalars instead of erroring out.
	* valprint.c (generic_value_print_capability): Make non-static and
	print additional space.
	* valprint.h (generic_value_print_capability): New prototype.

gdb/valprint.c
@@ -480,7 +480,7 @@ generic_value_print_ptr (struct value *val, struct ui_file *stream,
 
 /* generic_value_print helper for TYPE_CODE_CAPABILITY.  */
 
-static void
+void
 generic_value_print_capability (struct value *val, struct ui_file *stream,
 				const struct value_print_options *options)
 {
@@ -513,7 +513,7 @@ generic_value_print_capability (struct value *val, struct ui_file *stream,
       uint128_t dummy_cap;
       memcpy (&dummy_cap, contents, length);
       capability cap (dummy_cap, tag);
-      fprintf_filtered (stream, "%s", cap.to_str (true).c_str ());
+      fprintf_filtered (stream, "%s ", cap.to_str (true).c_str ());
     }
 
   return;

CHERI-Alliance/gdb

gdb/valprint.c
@@ -594,7 +594,7 @@ generic_value_print_ptr (struct value *val, struct ui_file *stream,
 
 /* generic_value_print helper for TYPE_CODE_CAPABILITY.  */
 
-static void
+void
 generic_value_print_capability (struct value *val, struct ui_file *stream,
 				const struct value_print_options *options)
 {
@@ -627,7 +627,7 @@ generic_value_print_capability (struct value *val, struct ui_file *stream,
       uint128_t dummy_cap;
       memcpy (&dummy_cap, contents, length);
       capability cap (dummy_cap, tag);
-      gdb_printf (stream, "%s", cap.to_str (true).c_str ());
+      gdb_printf (stream, "%s ", cap.to_str (true).c_str ());
     }
 
   return;