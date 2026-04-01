bminor/binutils-gdb

[General] Add support for capability types and modifier in GDB
This commit adds support for the __intcap_t and __uintcap_t types
in GDB. It also adds support for the __capability pointer modifier.

FIXME-Morello: The __intcap_t and __uintcap_t builtin types and the
capability data/function pointers still need to be properly defined.

A new testcase exercises the capability types and sizes.

gdb/ChangeLog:

2020-10-20  Luis Machado  <luis.machado@arm.com>

	* gdb/c-exp.y (CAPABILITY, INTCAP_KEYWORD, UINTCAP_KEYWORD): New
	tokens.
	Handle new tokens.
	* gdb/c-lang.c (c_primitive_types) <c_primitive_type_intcap_t>
	<c_primitive_type_uintcap_t>: New fields.
	(c_language_arch_info): Handle intcap/uintcap.
	(cplus_primitive_types) <cplus_primitive_type_intcap_t>
	<cplus_primitive_type_uintcap_t>: New fields.
	(cplus_language::language_arch_info): Handle intcap/uintcap.
	* gdb/c-typeprint.c (c_type_print_modifier): Handle the __capability
	modifier.
	* gdb/gdbtypes.c (make_capability_type): New function.
	(make_unqualified_type): Handle capabilities.
	(recursive_dump_type): Likewise.
	(gdbtypes_post_init): Initialize capability builtins.
	* gdb/gdbtypes.h (TYPE_INSTANCE_FLAG_CAPABILITY): New flag.
	(TYPE_CAPABILITY): New define.
	(struct type) <m_instance_flags>: New field.
	(struct builtin_type) <builtin_intcap_t, builtin_uintcap_t>: New
	fields.
	(make_capability_type): New prototype.
	* gdb/type-stack.c (type_stack::insert)
	(type_stack::follow_type_instance_flags)
	(type_stack::follow_types): Handle capability modifier.
	* gdb/type-stack.h (enum type_pieces) <tp_capability>: New field.

gdb/testsuite/ChangeLog:

2020-10-20  Luis Machado  <luis.machado@arm.com>

	* gdb.arch/aarch64-morello-captypes.c: New file.
	* gdb.arch/aarch64-morello-captypes.exp: New file.

gdb/c-typeprint.c
@@ -526,9 +526,18 @@ c_type_print_modifier (struct type *type, struct ui_file *stream,
       did_print_modifier = 1;
     }
 
+  if (TYPE_CAPABILITY (type))
+    {
+      if (did_print_modifier || need_pre_space)
+	fprintf_filtered (stream, " ");
+      fprintf_filtered (stream, "__capability");
+      did_print_modifier = 1;
+    }
+
   address_space_id
     = address_space_type_instance_flags_to_name (get_type_arch (type),
 						 type->instance_flags ());
+
   if (address_space_id)
     {
       if (did_print_modifier || need_pre_space)

gdb/gdbtypes.c
@@ -758,6 +758,23 @@ make_restrict_type (struct type *type)
 			      NULL);
 }
 
+/* Make a 'capability'-qualified version of TYPE.  */
+
+struct type *
+make_capability_type (struct type *type)
+{
+  struct type *ntype;
+  ntype = make_qualified_type (type,
+			       (type->instance_flags ()
+			        | TYPE_INSTANCE_FLAG_CAPABILITY),
+			       NULL);
+
+  /* Capability pointers are 128-bit.  */
+  TYPE_LENGTH (ntype) = 16;
+
+  return ntype;
+}
+
 /* Make a type without const, volatile, or restrict.  */
 
 struct type *
@@ -767,7 +784,8 @@ make_unqualified_type (struct type *type)
 			      (type->instance_flags ()
 			       & ~(TYPE_INSTANCE_FLAG_CONST
 				   | TYPE_INSTANCE_FLAG_VOLATILE
-				   | TYPE_INSTANCE_FLAG_RESTRICT)),
+				   | TYPE_INSTANCE_FLAG_RESTRICT
+				   | TYPE_INSTANCE_FLAG_CAPABILITY)),
 			      NULL);
 }
 
@@ -5063,6 +5081,10 @@ recursive_dump_type (struct type *type, int spaces)
     {
       puts_filtered (" TYPE_ATOMIC");
     }
+  if (TYPE_CAPABILITY (type))
+    {
+      puts_filtered (" TYPE_CAPABILITY");
+    }
   puts_filtered ("\n");
 
   printfi_filtered (spaces, "flags");
@@ -5865,6 +5887,18 @@ gdbtypes_post_init (struct gdbarch *gdbarch)
     = lookup_pointer_type (lookup_function_type (builtin_type->builtin_void));
   builtin_type->builtin_func_func
     = lookup_function_type (builtin_type->builtin_func_ptr);
+
+  /* Capability types.  */
+  builtin_type->builtin_intcap_t
+    = arch_integer_type (gdbarch, 128, 0, "__intcap_t");
+  builtin_type->builtin_uintcap_t
+    = arch_integer_type (gdbarch, 128, 1, "__uintcap_t");
+
+  /* Capability pointer types.  */
+  builtin_type->builtin_data_addr_capability
+    = lookup_pointer_type (builtin_type->builtin_intcap_t);
+  builtin_type->builtin_code_addr_capability
+    = lookup_pointer_type (lookup_function_type (builtin_type->builtin_void));
 
   /* This type represents a GDB internal function.  */
   builtin_type->internal_fn

CHERI-Alliance/gdb

gdb/c-typeprint.c
@@ -485,9 +485,18 @@ c_type_print_modifier (struct type *type, struct ui_file *stream,
       did_print_modifier = 1;
     }
 
+  if (TYPE_CAPABILITY (type))
+    {
+      if (did_print_modifier || need_pre_space)
+	gdb_printf (stream, " ");
+      gdb_printf (stream, "__capability");
+      did_print_modifier = 1;
+    }
+
   address_space_id
     = address_space_type_instance_flags_to_name (type->arch (),
 						 type->instance_flags ());
+
   if (address_space_id)
     {
       if (did_print_modifier || need_pre_space)

gdb/gdbtypes.c
@@ -791,6 +791,23 @@ make_restrict_type (struct type *type)
 			      NULL);
 }
 
+/* Make a 'capability'-qualified version of TYPE.  */
+
+struct type *
+make_capability_type (struct type *type)
+{
+  struct type *ntype;
+  ntype = make_qualified_type (type,
+			       (type->instance_flags ()
+			        | TYPE_INSTANCE_FLAG_CAPABILITY),
+			       NULL);
+
+  /* Capability pointers are 128-bit.  */
+  ntype->set_length (16);
+
+  return ntype;
+}
+
 /* Make a type without const, volatile, or restrict.  */
 
 struct type *
@@ -800,7 +817,8 @@ make_unqualified_type (struct type *type)
 			      (type->instance_flags ()
 			       & ~(TYPE_INSTANCE_FLAG_CONST
 				   | TYPE_INSTANCE_FLAG_VOLATILE
-				   | TYPE_INSTANCE_FLAG_RESTRICT)),
+				   | TYPE_INSTANCE_FLAG_RESTRICT
+				   | TYPE_INSTANCE_FLAG_CAPABILITY)),
 			      NULL);
 }
 
@@ -5297,6 +5315,10 @@ recursive_dump_type (struct type *type, int spaces)
     {
       gdb_puts (" TYPE_ATOMIC");
     }
+  if (TYPE_CAPABILITY (type))
+    {
+      gdb_puts (" TYPE_CAPABILITY");
+    }
   gdb_puts ("\n");
 
   gdb_printf ("%*sflags", spaces, "");
@@ -6095,6 +6117,18 @@ create_gdbtypes_data (struct gdbarch *gdbarch)
     = lookup_pointer_type (lookup_function_type (builtin_type->builtin_void));
   builtin_type->builtin_func_func
     = lookup_function_type (builtin_type->builtin_func_ptr);
+
+  /* Capability types.  */
+  builtin_type->builtin_intcap_t
+    = init_integer_type (alloc, 128, 0, "__intcap_t");
+  builtin_type->builtin_uintcap_t
+    = init_integer_type (alloc, 128, 1, "__uintcap_t");
+
+  /* Capability pointer types.  */
+  builtin_type->builtin_data_addr_capability
+    = lookup_pointer_type (builtin_type->builtin_void);
+  builtin_type->builtin_code_addr_capability
+    = lookup_pointer_type (lookup_function_type (builtin_type->builtin_void));
 
   /* This type represents a GDB internal function.  */
   builtin_type->internal_fn