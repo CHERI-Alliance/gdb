bminor/binutils-gdb

Print shorter version of decoded capabilities
In order to produce output that is not so verbose, we print the short version
of the decoded capabilities instead.

gdb/ChangeLog:

2020-11-11  Luis Machado  <luis.machado@arm.com>

	* valprint.c (generic_value_print_capability): Use compact form.

gdbsupport/ChangeLog:

2020-11-11  Luis Machado  <luis.machado@arm.com>

	* capability.cc (cap_short_perms_strings): New static global.
	(capability::to_str): New argument COMPACT. Print contents based
	on the new argument.
	(capability::print): Update calls.
	* capability.h (capability::to_str): Update prototype and
	documentation.

gdb/valprint.c
@@ -513,7 +513,7 @@ generic_value_print_capability (struct value *val, struct ui_file *stream,
       uint128_t dummy_cap;
       memcpy (&dummy_cap, contents, length);
       capability cap (dummy_cap, tag);
-      fprintf_filtered (stream, "%s", cap.to_str ().c_str ());
+      fprintf_filtered (stream, "%s", cap.to_str (true).c_str ());
     }
 
   return;

CHERI-Alliance/gdb

gdb/valprint.c
@@ -627,7 +627,7 @@ generic_value_print_capability (struct value *val, struct ui_file *stream,
       uint128_t dummy_cap;
       memcpy (&dummy_cap, contents, length);
       capability cap (dummy_cap, tag);
-      gdb_printf (stream, "%s", cap.to_str ().c_str ());
+      gdb_printf (stream, "%s", cap.to_str (true).c_str ());
     }
 
   return;