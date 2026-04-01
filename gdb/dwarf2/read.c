bminor/binutils-gdb

[General] More capability type handling (merge with others)
Teach more parts of GDB how to handle capabilities properly, add a function
to print capabilities in their natural format and initialize capability types
properly.

gdb/ChangeLog:

2020-10-20  Luis Machado  <luis.machado@arm.com>

	* c-typeprint.c (c_type_print_varspec_prefix)
	(c_type_print_varspec_suffix): Handle capability type.
	* dwarf2/read.c (read_base_type): Call init_capability_type for
	capabilities.
	* gdbtypes.c (init_capability_type): New function.
	(type_align): Handle capability type.
	(recursive_dump_type): Likewise.
	(arch_capability_type): New function.
	(gdbtypes_post_init): Call arch_capability_type for capability
	types.
	* gdbtypes.h (init_capability_type, arch_capability_type):  New
	prototypes.
	* valprint.c: Include gdbsupport/capability.h.
	(generic_value_print_capability): New function.
	(generic_value_print): Handle capability types.

gdb/dwarf2/read.c
@@ -18025,13 +18025,11 @@ read_base_type (struct die_info *die, struct dwarf2_cu *cu)
 	break;
 
       case DW_ATE_CHERI_signed_intcap:
-      case DW_ATE_CHERI_unsigned_intcap:
-	{
-	  /* Turn DW_ATE_CHERI_*_intcap into a void * pointer.  */
-	  type = init_type (objfile, TYPE_CODE_VOID, TARGET_CHAR_BIT, NULL);
-	  type = init_pointer_type (objfile, bits, name, type);
+	  type = init_capability_type (objfile, bits, false, name);
 	  break;
-	}
+      case DW_ATE_CHERI_unsigned_intcap:
+	  type = init_capability_type (objfile, bits, true, name);
+	  break;
 
       default:
 	complaint (_("unsupported DW_AT_encoding: '%s'"),

CHERI-Alliance/gdb

gdb/dwarf2/read.c
@@ -13878,13 +13878,11 @@ read_base_type (struct die_info *die, struct dwarf2_cu *cu)
 	break;
 
       case DW_ATE_CHERI_signed_intcap:
-      case DW_ATE_CHERI_unsigned_intcap:
-	{
-	  /* Turn DW_ATE_CHERI_*_intcap into a void * pointer.  */
-	  type = alloc.new_type (TYPE_CODE_VOID, TARGET_CHAR_BIT, NULL);
-	  type = init_pointer_type (alloc, bits, name, type);
+	  type = init_capability_type (alloc, bits, false, name);
 	  break;
-	}
+      case DW_ATE_CHERI_unsigned_intcap:
+	  type = init_capability_type (alloc, bits, true, name);
+	  break;
 
       default:
 	complaint (_("unsupported DW_AT_encoding: '%s'"),