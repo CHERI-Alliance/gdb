bminor/binutils-gdb

Support assignment of capabilities to C registers
Enable assignment of capabilities to C registers while preserving
the capability tag. This enables operations like the following:

set $c0=$c1
set $c0=p, where "p" is a capability (pointer) in AARCH64-CAP

Due to the X/C register overlap, we also force a re-read of register
data after every register write. So any 'G' packets are immediately
followed by a 'g' packet.

gdb/valops.c
@@ -1175,10 +1175,9 @@ value_assign (struct value *toval, struct value *fromval)
 	      }
 	    else
 	      {
-		put_frame_register_bytes (frame, value_reg,
+		put_frame_register_value (frame, value_reg,
 					  value_offset (toval),
-					  TYPE_LENGTH (type),
-					  value_contents (fromval));
+					  fromval);
 	      }
 	  }
 

CHERI-Alliance/gdb

gdb/valops.c
@@ -1265,9 +1265,9 @@ value_assign (struct value *toval, struct value *fromval)
 					   fromval->contents ().data ());
 	      }
 	    else
-	      put_frame_register_bytes (next_frame, value_reg,
+	      put_frame_register_value (next_frame, value_reg,
 					toval->offset (),
-					fromval->contents ());
+					fromval);
 	  }
 
 	gdb::observers::register_changed.notify