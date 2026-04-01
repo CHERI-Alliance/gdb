bminor/binutils-gdb

Enable capability writes to memory
Enable writing/forging capabilities to memory via the PTRACE_POKECAP ptrace
request.  This patch enables both GDB and gdbserver for Morello.

For remote writes, we use the qXfer:capa:write packet.

gdb/ChangeLog:

2021-03-17  Luis Machado  <luis.machado@arm.com>

	* aarch64-linux-nat.c (aarch64_linux_nat_target)
	<write_capability>: New member function override.
	(aarch64_linux_nat_target::write_capability): New member
	function.
	(maint_print_cap_from_addr_cmd): Adjust printing format.
	(maint_set_capability_in_memory_cmd): New maintenance function.
	(add_show_debug_regs_command): Register new maintenance command.
	* nat/aarch64-cap-linux.c (aarch64_linux_write_capability): New
	function.
	* nat/aarch64-cap-linux.h (aarch64_linux_write_capability): New
	prototype.
	* remote.c (remote_target)
	<write_capability>: New member function override.
	Adjust enum documentation for PACKET_qXfer_capability.
	(remote_target::write_capability): New member function.
	(_initialize_remote): Adjust documentation for
	PACKET_qXfer_capability.
	* target-debug.h
	(target_debug_print_gdb_array_view_const_gdb_byte): New macro.
	* target-delegates.c: Regenerate.
	* target.c (target_write_capability): New function.
	* target.h (struct target_ops) <write_capability>: New virtual member
	function.
	(target_write_capability): New prototype.

gdbserver/ChangeLog:

2021-03-17  Luis Machado  <luis.machado@arm.com>

	* linux-aarch64-low.cc (aarch64_target::qxfer_capability): Handle
	capability writes.
	* server.cc (handle_qxfer_capability): Likewise.

gdb/aarch64-linux-nat.c
@@ -103,6 +103,8 @@ public:
 
   struct gdbarch *thread_architecture (ptid_t) override;
   gdb::byte_vector read_capability (CORE_ADDR addr) override;
+  bool write_capability (CORE_ADDR addr,
+			 gdb::array_view<const gdb_byte> buffer) override;
 };
 
 static aarch64_linux_nat_target the_aarch64_linux_nat_target;
@@ -1107,6 +1109,25 @@ aarch64_linux_nat_target::read_capability (CORE_ADDR addr)
   return cap_vec;
 }
 
+/* Implement the "write_capability" target_ops method.  */
+
+bool
+aarch64_linux_nat_target::write_capability (CORE_ADDR addr,
+					    gdb::array_view<const gdb_byte> buffer)
+{
+  int tid = get_ptrace_pid (inferior_ptid);
+
+  struct user_cap cap;
+
+  memcpy (&cap.tag, buffer.data (), 1);
+  memcpy (&cap.val, buffer.data () + 1, 16);
+
+  if (!aarch64_linux_write_capability (tid, addr, cap))
+    perror_with_name (_("Unable to write capability from address."));
+
+  return true;
+}
+
 /* Implement the maintenance print capability tag command.  */
 
 static void
@@ -1117,9 +1138,45 @@ maint_print_cap_from_addr_cmd (const char *args, int from_tty)
   cap = target_read_capability (addr);
 
   for (auto it : cap)
-    fprintf_unfiltered (gdb_stdlog, "%x ", it);
+    fprintf_unfiltered (gdb_stdlog, "%02x ", it);
 
   fputs_unfiltered ("\n", gdb_stdlog);
+}
+
+/* Implement the maintenance set capability in memory command.  */
+
+static void
+maint_set_capability_in_memory_cmd (const char *args, int from_tty)
+{
+  std::string addr_str, tag_str, upper_str, lower_str;
+  const char *args_ptr = args;
+
+  addr_str = extract_string_maybe_quoted (&args_ptr);
+  tag_str = extract_string_maybe_quoted (&args_ptr);
+  upper_str = extract_string_maybe_quoted (&args_ptr);
+  lower_str = extract_string_maybe_quoted (&args_ptr);
+
+  CORE_ADDR addr = parse_and_eval_address (addr_str.c_str ());
+  CORE_ADDR tag_part = parse_and_eval_address (tag_str.c_str ());
+  CORE_ADDR half_a = parse_and_eval_address (upper_str.c_str ());
+  CORE_ADDR half_b = parse_and_eval_address (lower_str.c_str ());
+
+  unsigned __int128 a, b;
+
+  a = half_a;
+  b = half_b;
+
+  a = (a << 64) | b;
+  bool tag = (tag_part != 0)? true : false;
+
+  gdb::byte_vector cap;
+
+  cap.resize (17);
+  memcpy (cap.data (), &tag, 1);
+  memcpy (cap.data () + 1, &a, 16);
+
+  if (!target_write_capability (addr, {cap.data (), cap.size ()}))
+    perror_with_name (_("Failed to set capability in memory."));
 }
 
 /* Define AArch64 maintenance commands.  */
@@ -1145,6 +1202,10 @@ triggers a breakpoint or watchpoint."),
   add_cmd ("cap_from_addr", class_maintenance, maint_print_cap_from_addr_cmd, _("\
 Print the capability from addr."),
 		 &maintenanceprintlist);
+
+  add_cmd ("cap_in_memory", class_maintenance,
+	   maint_set_capability_in_memory_cmd,
+	   _("Print the capability from addr."), &maintenancelist);
 }
 
 void _initialize_aarch64_linux_nat ();

CHERI-Alliance/gdb

gdb/aarch64-linux-nat.c
@@ -112,6 +112,8 @@ public:
   bool is_address_tagged (gdbarch *gdbarch, CORE_ADDR address) override;
 
   gdb::byte_vector read_capability (CORE_ADDR addr) override;
+  bool write_capability (CORE_ADDR addr,
+			 gdb::array_view<const gdb_byte> buffer) override;
 };
 
 static aarch64_linux_nat_target the_aarch64_linux_nat_target;
@@ -1269,6 +1271,25 @@ aarch64_linux_nat_target::read_capability (CORE_ADDR addr)
   return cap_vec;
 }
 
+/* Implement the "write_capability" target_ops method.  */
+
+bool
+aarch64_linux_nat_target::write_capability (CORE_ADDR addr,
+					    gdb::array_view<const gdb_byte> buffer)
+{
+  int tid = get_ptrace_pid (inferior_ptid);
+
+  struct user_cap cap;
+
+  memcpy (&cap.tag, buffer.data (), 1);
+  memcpy (&cap.val, buffer.data () + 1, 16);
+
+  if (!aarch64_linux_write_capability (tid, addr, cap))
+    perror_with_name (_("Unable to write capability from address."));
+
+  return true;
+}
+
 /* Implement the maintenance print capability tag command.  */
 
 static void
@@ -1279,9 +1300,45 @@ maint_print_cap_from_addr_cmd (const char *args, int from_tty)
   cap = target_read_capability (addr);
 
   for (auto it : cap)
-    gdb_printf (gdb_stdlog, "%x ", it);
+    gdb_printf (gdb_stdlog, "%02x ", it);
 
   gdb_puts ("\n", gdb_stdlog);
+}
+
+/* Implement the maintenance set capability in memory command.  */
+
+static void
+maint_set_capability_in_memory_cmd (const char *args, int from_tty)
+{
+  std::string addr_str, tag_str, upper_str, lower_str;
+  const char *args_ptr = args;
+
+  addr_str = extract_string_maybe_quoted (&args_ptr);
+  tag_str = extract_string_maybe_quoted (&args_ptr);
+  upper_str = extract_string_maybe_quoted (&args_ptr);
+  lower_str = extract_string_maybe_quoted (&args_ptr);
+
+  CORE_ADDR addr = parse_and_eval_address (addr_str.c_str ());
+  CORE_ADDR tag_part = parse_and_eval_address (tag_str.c_str ());
+  CORE_ADDR half_a = parse_and_eval_address (upper_str.c_str ());
+  CORE_ADDR half_b = parse_and_eval_address (lower_str.c_str ());
+
+  unsigned __int128 a, b;
+
+  a = half_a;
+  b = half_b;
+
+  a = (a << 64) | b;
+  bool tag = (tag_part != 0)? true : false;
+
+  gdb::byte_vector cap;
+
+  cap.resize (17);
+  memcpy (cap.data (), &tag, 1);
+  memcpy (cap.data () + 1, &a, 16);
+
+  if (!target_write_capability (addr, {cap.data (), cap.size ()}))
+    perror_with_name (_("Failed to set capability in memory."));
 }
 
 /* Implement the "read_capability" target_ops method.  */
@@ -1303,6 +1360,25 @@ aarch64_linux_nat_target::read_capability (CORE_ADDR addr)
   return cap_vec;
 }
 
+/* Implement the "write_capability" target_ops method.  */
+
+bool
+aarch64_linux_nat_target::write_capability (CORE_ADDR addr,
+					    gdb::array_view<const gdb_byte> buffer)
+{
+  int tid = get_ptrace_pid (inferior_ptid);
+
+  struct user_cap cap;
+
+  memcpy (&cap.tag, buffer.data (), 1);
+  memcpy (&cap.val, buffer.data () + 1, 16);
+
+  if (!aarch64_linux_write_capability (tid, addr, cap))
+    perror_with_name (_("Unable to write capability from address."));
+
+  return true;
+}
+
 /* Implement the maintenance print capability tag command.  */
 
 static void
@@ -1313,9 +1389,45 @@ maint_print_cap_from_addr_cmd (const char *args, int from_tty)
   cap = target_read_capability (addr);
 
   for (auto it : cap)
-    gdb_printf (gdb_stdlog, "%x ", it);
+    gdb_printf (gdb_stdlog, "%02x ", it);
 
   gdb_puts ("\n", gdb_stdlog);
+}
+
+/* Implement the maintenance set capability in memory command.  */
+
+static void
+maint_set_capability_in_memory_cmd (const char *args, int from_tty)
+{
+  std::string addr_str, tag_str, upper_str, lower_str;
+  const char *args_ptr = args;
+
+  addr_str = extract_string_maybe_quoted (&args_ptr);
+  tag_str = extract_string_maybe_quoted (&args_ptr);
+  upper_str = extract_string_maybe_quoted (&args_ptr);
+  lower_str = extract_string_maybe_quoted (&args_ptr);
+
+  CORE_ADDR addr = parse_and_eval_address (addr_str.c_str ());
+  CORE_ADDR tag_part = parse_and_eval_address (tag_str.c_str ());
+  CORE_ADDR half_a = parse_and_eval_address (upper_str.c_str ());
+  CORE_ADDR half_b = parse_and_eval_address (lower_str.c_str ());
+
+  unsigned __int128 a, b;
+
+  a = half_a;
+  b = half_b;
+
+  a = (a << 64) | b;
+  bool tag = (tag_part != 0)? true : false;
+
+  gdb::byte_vector cap;
+
+  cap.resize (17);
+  memcpy (cap.data (), &tag, 1);
+  memcpy (cap.data () + 1, &a, 16);
+
+  if (!target_write_capability (addr, {cap.data (), cap.size ()}))
+    perror_with_name (_("Failed to set capability in memory."));
 }
 
 bool
@@ -1338,6 +1450,10 @@ INIT_GDB_FILE (aarch64_linux_nat)
   add_cmd ("cap_from_addr", class_maintenance, maint_print_cap_from_addr_cmd, _("\
 Print the capability from addr."),
 	   &maintenanceprintlist);
+
+  add_cmd ("cap_in_memory", class_maintenance,
+	   maint_set_capability_in_memory_cmd,
+	   _("Print the capability from addr."), &maintenancelist);
 
   /* Register the target.  */
   linux_target = &the_aarch64_linux_nat_target;