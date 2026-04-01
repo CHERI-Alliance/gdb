bminor/binutils-gdb

[Morello] Loads and stores with alternate base
These are loads that use a capability base register in A64 and 64-bit
integer register in C64.

This patch implements LDAR, LDARB, STLR and STLRB.

gas/ChangeLog:

2020-10-20  Siddhesh Poyarekar  <siddesh.poyarekar@arm.com>

	* config/tc-aarch64.c (parse_operands): Add Wt.
	* testsuite/gas/aarch64/morello_ldst-c64.d: Add tests.
	* testsuite/gas/aarch64/morello_ldst.d: Likewise.
	* testsuite/gas/aarch64/morello_ldst.s: Likewise.

include/ChangeLog:

2020-10-20  Siddhesh Poyarekar  <siddesh.poyarekar@arm.com>

	* opcode/aarch64.h (aarch64_opnd): Add Wt.

opcodes/ChangeLog:

2020-10-20  Siddhesh Poyarekar  <siddesh.poyarekar@arm.com>

	* aarch64-opc.c (get_altbase_reg_name): New function.
	(aarch64_print_operand): Use it.  Add Wt.
	* aarch64-tbl.h (QL2_A64C_W_CAPADDR): New macro.
	(aarch64_opcode_table): Add instructions.
	(AARCH64_OPERANDS): New operand.
	* aarch64-asm-2.c: Regenerate.
	* aarch64-dis-2.c: Regenerate.
	* aarch64-opc-2.c: Regenerate.

gas/config/tc-aarch64.c
@@ -5761,6 +5761,10 @@ parse_operands (char *str, const aarch64_opcode *opcode)
 
       switch (operands[i])
 	{
+	case AARCH64_OPND_Wt:
+	  po_int_reg_or_fail (REG_TYPE_R_32);
+	  break;
+
 	case AARCH64_OPND_Rd:
 	case AARCH64_OPND_Rn:
 	case AARCH64_OPND_Rm:

opcodes/aarch64-opc.c
@@ -3075,6 +3075,17 @@ get_cap_reg_name (int regno, int sp_reg_p)
 }
 
 static inline const char *
+get_altbase_reg_name (aarch64_feature_set features, int regno, int sp_reg_p,
+		      const aarch64_opcode *opcode)
+{
+  if (AARCH64_CPU_HAS_FEATURE(features, AARCH64_FEATURE_C64)
+      && opcode->iclass != br_capaddr)
+    return get_64bit_int_reg_name (regno, sp_reg_p);
+  else
+    return get_cap_reg_name (regno, sp_reg_p);
+}
+
+static inline const char *
 get_base_reg_name (aarch64_feature_set features, int regno, int sp_reg_p)
 {
   if (AARCH64_CPU_HAS_FEATURE(features, AARCH64_FEATURE_C64))
@@ -3348,6 +3359,7 @@ aarch64_print_operand (char *buf, size_t size, bfd_vma pc,
     case AARCH64_OPND_Rn:
     case AARCH64_OPND_Rm:
     case AARCH64_OPND_Rt:
+    case AARCH64_OPND_Wt:
     case AARCH64_OPND_Rt2:
     case AARCH64_OPND_Rs:
     case AARCH64_OPND_Ra:
@@ -3795,7 +3807,7 @@ aarch64_print_operand (char *buf, size_t size, bfd_vma pc,
 
     case AARCH64_OPND_CAPADDR_SIMPLE:
       snprintf (buf, size, "[%s]",
-		get_cap_reg_name (opnd->addr.base_regno, 1));
+		get_altbase_reg_name (features, opnd->addr.base_regno, 1, opcode));
       break;
 
     case AARCH64_OPND_ADDR_SIMPLE:

CHERI-Alliance/gdb

gas/config/tc-aarch64.c
@@ -6927,6 +6927,10 @@ parse_operands (char *str, const aarch64_opcode *opcode)
 
       switch (operands[i])
 	{
+	case AARCH64_OPND_Wt:
+	  po_int_fp_reg_or_fail (REG_TYPE_R_32);
+	  break;
+
 	case AARCH64_OPND_Rd:
 	case AARCH64_OPND_Rn:
 	case AARCH64_OPND_Rm:

opcodes/aarch64-opc.c
@@ -3847,6 +3847,17 @@ get_cap_reg_name (int regno, int sp_reg_p)
 }
 
 static inline const char *
+get_altbase_reg_name (aarch64_feature_set features, int regno, int sp_reg_p,
+		      const aarch64_opcode *opcode)
+{
+  if (AARCH64_CPU_HAS_FEATURE(features, C64)
+      && opcode->iclass != br_capaddr)
+    return get_64bit_int_reg_name (regno, sp_reg_p);
+  else
+    return get_cap_reg_name (regno, sp_reg_p);
+}
+
+static inline const char *
 get_base_reg_name (aarch64_feature_set features, int regno, int sp_reg_p)
 {
   if (AARCH64_CPU_HAS_FEATURE(features, C64))
@@ -4291,6 +4302,7 @@ aarch64_print_operand (char *buf, size_t size, bfd_vma pc,
     case AARCH64_OPND_Rn:
     case AARCH64_OPND_Rm:
     case AARCH64_OPND_Rt:
+    case AARCH64_OPND_Wt:
     case AARCH64_OPND_Rt2:
     case AARCH64_OPND_Rs:
     case AARCH64_OPND_Ra:
@@ -5033,7 +5045,7 @@ aarch64_print_operand (char *buf, size_t size, bfd_vma pc,
 
     case AARCH64_OPND_CAPADDR_SIMPLE:
       snprintf (buf, size, "[%s]",
-		get_cap_reg_name (opnd->addr.base_regno, 1));
+		get_altbase_reg_name (features, opnd->addr.base_regno, 1, opcode));
       break;
 
     case AARCH64_OPND_ADDR_SIMPLE: