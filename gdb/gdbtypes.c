bminor/binutils-gdb

Support for passing/returning parameters
This patch implements passing/returning of parameters for Morello.  It is
implemented based on the Morello AAPCS64 document and supports both the hybrid
AAPCS64 and Pure-Cap AAPCS64-CAP ABI's.

gdb/ChangeLog

2021-02-26  Luis Machado  <luis.machado@arm.com>

	* aarch64-tdep.c (struct stack_item_t) <arg_value): New field.
	(set_register_tag, pass_in_c, pass_in_c_or_stack,
	pass_in_c_x_or_stack, type_fields_overlap_capabilities,
	convert_pointer_to_capability,
	morello_write_memory_with_capabilities,
	morello_push_dummy_call, morello_extract_return_value,
	morello_return_in_memory, morello_store_return_value,
	morello_return_value): New functions.
	(pass_in_x, pass_in_v, pass_on_stack, pass_in_x_or_stack,
	pass_in_v_vfp_candidate): Update.
	(aarch64_return_value): New argument value.
	(aarch64_gdbarch_init): Conditionally set both gdbarch_push_dummy_call
	and gdbarch_return_value.
	* arch/aarch64.h (MORELLO_MEMORY_TAG_GRANULE_SIZE): New constant.
	* dwarf2/read.c (handle_struct_member_die): Record whether the
	composite type contains capabilities.
	* gdbarch.sh (return_value): New argument value.
	* gdbarch.c: Regenerate.
	* gdbarch.h: Regenerate.
	* gdbtypes.c (recursive_dump_type): Dump extra information.
	(append_composite_type_field_raw): Set whether the composite type
	constains capabilities.
	* gdbtypes.h (struct main_type) <m_flag_contains_capability>: New field.
	<contains_capability>: New member function.
	<set_contains_capability>: New member function.

	* alpha-tdep.c: Update return_value hook prototype.
	* amd64-tdep.c: Update return_value hook prototype.
	* amd64-windows-tdep.c: Update return_value hook prototype.
	* arc-tdep.c: Update return_value hook prototype.
	* arm-tdep.c: Update return_value hook prototype.
	* avr-tdep.c: Update return_value hook prototype.
	* bfin-tdep.c: Update return_value hook prototype.
	* bpf-tdep.c: Update return_value hook prototype.
	* cris-tdep.c: Update return_value hook prototype

gdb/gdbtypes.c
@@ -5120,6 +5120,10 @@ recursive_dump_type (struct type *type, int spaces)
     {
       puts_filtered (" TYPE_ENDIANITY_NOT_DEFAULT");
     }
+  if (type->contains_capability ())
+    {
+      puts_filtered (" TYPE_CONTAINS_CAPABILITY");
+    }
   if (type->is_stub ())
     {
       puts_filtered (" TYPE_STUB");
@@ -5727,6 +5731,11 @@ append_composite_type_field_raw (struct type *t, const char *name,
   memset (f, 0, sizeof f[0]);
   f[0].set_type (field);
   FIELD_NAME (f[0]) = name;
+
+  if (field->contains_capability ()
+      || field->code () == TYPE_CODE_CAPABILITY)
+    t->set_contains_capability (true);
+
   return f;
 }
 

CHERI-Alliance/gdb

Co-Authored-By: John Baldwin <jhb@FreeBSD.org>

gdb/gdbtypes.c
@@ -5349,6 +5349,10 @@ recursive_dump_type (struct type *type, int spaces)
     {
       gdb_puts (" TYPE_ENDIANITY_NOT_DEFAULT");
     }
+  if (type->contains_capability ())
+    {
+      gdb_puts (" TYPE_CONTAINS_CAPABILITY");
+    }
   if (type->is_stub ())
     {
       gdb_puts (" TYPE_STUB");
@@ -5791,6 +5795,11 @@ append_composite_type_field_raw (struct type *t, const char *name,
   memset (f, 0, sizeof f[0]);
   f[0].set_type (field);
   f[0].set_name (name);
+
+  if (field->contains_capability ()
+      || field->code () == TYPE_CODE_CAPABILITY)
+    t->set_contains_capability (true);
+
   return f;
 }
 