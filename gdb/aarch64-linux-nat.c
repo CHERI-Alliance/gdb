bminor/binutils-gdb

[Morello GDB] Fix bug in conditional definition of morello structs
The previous commit fixing this issue (bf5ddcecc07c2d89e824851f5f940ebe7e2af0fd)
failed to spot an issue with include ordering.

Fix all such issues with this patch.

gdb/aarch64-linux-nat.c
@@ -33,10 +33,6 @@
 #include "aarch32-tdep.h"
 #include "arch/arm.h"
 #include "arch/aarch64-cap-linux.h"
-#include "nat/aarch64-cap-linux.h"  /* For Morello */
-#include "nat/aarch64-linux.h"
-#include "nat/aarch64-linux-hw-point.h"
-#include "nat/aarch64-sve-linux-ptrace.h"
 
 #include "elf/external.h"
 #include "elf/common.h"
@@ -51,6 +47,11 @@
 /* Defines ps_err_e, struct ps_prochandle.  */
 #include "gdb_proc_service.h"
 #include "arch-utils.h"
+
+#include "nat/aarch64-cap-linux.h"  /* For Morello */
+#include "nat/aarch64-linux.h"
+#include "nat/aarch64-linux-hw-point.h"
+#include "nat/aarch64-sve-linux-ptrace.h"
 
 #ifndef TRAP_HWBKPT
 #define TRAP_HWBKPT 0x0004

CHERI-Alliance/gdb

gdb/aarch64-linux-nat.c
@@ -33,10 +33,6 @@
 #include "aarch32-tdep.h"
 #include "arch/arm.h"
 #include "arch/aarch64-cap-linux.h"
-#include "nat/aarch64-cap-linux.h"  /* For Morello */
-#include "nat/aarch64-linux.h"
-#include "nat/aarch64-linux-hw-point.h"
-#include "nat/aarch64-scalable-linux-ptrace.h"
 
 #include "elf/external.h"
 #include "elf/common.h"
@@ -60,6 +56,11 @@
 #include "arch/aarch64-scalable-linux.h"
 
 #include <string.h>
+
+#include "nat/aarch64-cap-linux.h"  /* For Morello */
+#include "nat/aarch64-linux.h"
+#include "nat/aarch64-linux-hw-point.h"
+#include "nat/aarch64-scalable-linux-ptrace.h"
 
 #ifndef TRAP_HWBKPT
 #define TRAP_HWBKPT 0x0004