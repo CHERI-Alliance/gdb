1c09c6b6292ace6170e5134221e040b56cc6d552
John Baldwin <jhb@FreeBSD.org>
2026-05-05 14:13:01 -0400

    Allow capability types to be used with (extract|store)_typed_address
    These are used when casting to/from [u]intcap_t in unpack_long and
    pack_[unsigned_]long.

--- gdb/extract-store-integer.c
+++ gdb/extract-store-integer.c
@@ -72,7 +72,7 @@ template ULONGEST extract_integer<ULONGEST>
 CORE_ADDR
 extract_typed_address (const gdb_byte *buf, struct type *type)
 {
-  gdb_assert (type->is_pointer_or_reference ());
+  gdb_assert (type->is_pointer_or_reference () || is_capability (type));
   return gdbarch_pointer_to_address (type->arch (), type, buf);
 }
 
@@ -119,7 +119,7 @@ template void store_integer (gdb::array_view<gdb_byte> dst,
 void
 store_typed_address (gdb_byte *buf, struct type *type, CORE_ADDR addr)
 {
-  gdb_assert (type->is_pointer_or_reference ());
+  gdb_assert (type->is_pointer_or_reference () || is_capability (type));
   gdbarch_address_to_pointer (type->arch (), type, buf, addr);
 }
 
